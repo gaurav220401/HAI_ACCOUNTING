8bf810d689622ddc9c4218fca22f61164b5a47a6
someshsrichandan <someshsrichandan@gmail.com>
2026-02-26 14:42:12 +0530

    feat: add AlertDialog component with various subcomponents for UI alerts

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import { QueryProvider } from "@/contexts/query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          <AuthProvider>{children}</AuthProvider>
+          <AuthProvider>
+            {children}
+            <Toaster/>
+          </AuthProvider>
         </QueryProvider>
       </body>
     </html>
