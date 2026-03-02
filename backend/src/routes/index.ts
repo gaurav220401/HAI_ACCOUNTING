b127dbbd566950979b22f5a6e7c69d4edbb5201f
Batman-0001 <santrasuraj14@gmail.com>
2026-03-02 14:04:39 +0530

    add the quotes page

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -8,6 +8,7 @@ import contactRoutes from "./contact.routes";
 import itemRoutes from "./item.routes";
 import currencyRoutes from "./currency.routes";
 import settingsRoutes from "./settings.routes";
+import quoteRoutes from "./quote.routes";
 
 const router = Router();
 
@@ -20,5 +21,6 @@ router.use("/contacts", contactRoutes);
 router.use("/items", itemRoutes);
 router.use("/currencies", currencyRoutes);
 router.use("/settings", settingsRoutes);
+router.use("/quotes", quoteRoutes);
 
 export default router;
